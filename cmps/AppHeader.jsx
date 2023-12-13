d6d403b0c95979b73f8c11011289cb69ed5b6753
Michael Lulev <miki1990@gmail.com>
2023-12-13 12:49:36 +0200

    Rename class full to full-width

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -2,16 +2,18 @@ const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
 
-    return <header className="app-header full">
-        <Link className="user-img-container" to="/">
-            {/* <h3>LOGO!</h3> */}
-            <img className="user-img" src="assets/img/icons/user-img.png" alt="" />
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
-    </header>
+    return (
+        <header className="app-header full-width">
+            <Link className="user-img-container" to="/">
+                {/* <h3>LOGO!</h3> */}
+                <img className="user-img" src="assets/img/icons/user-img.png" alt="" />
+            </Link>
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/mail">Mail</NavLink>
+                <NavLink to="/note">Note</NavLink>
+            </nav>
+        </header>
+    )
 }
