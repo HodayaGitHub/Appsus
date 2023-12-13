0acbbca864cefd2f5d0c3aeb267568e30b64c79c
HodayaGitHub <hodaya1812@gmail.com>
2023-12-13 14:58:07 +0200

    Add icons and css file for the emails app

--- apps/mail/cmps/EmailList.jsx
+++ apps/mail/cmps/EmailList.jsx
@@ -17,10 +17,13 @@ export function EmailList({ emails, onRemoveEmail }) {
         // <ul {...ulProps} >
         <ul>
             {emails.map(email =>
-                <li class="clean-list" key={email.id}>
+                <li className="clean-list" key={email.id}>
                     <EmailPreview email={email} />
                     <section>
-                        <button onClick={() => onRemoveEmail(email.id)}>Remove email</button>
+                        <button className="email-btn" onClick={() => onRemoveEmail(email.id)}>
+                            <img src="../../assets/img/icons/email-icons/trash.svg" alt="Remove email" />
+                        </button>
+                        {/* <button onClick={() => onRemoveEmail(email.id)}>Remove email</button> */}
                         {/* <button><Link to={`/email/${email.id}`}>Details</Link></button> */}
                         {/* <button><Link to={`/email/edit/${email.id}`}>Edit</Link></button> */}
                     </section>
