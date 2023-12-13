f543be312feb9689d03cf6e535807da5bbac2fa9
Michael Lulev <miki1990@gmail.com>
2023-12-13 17:44:34 +0200

    Add create and delete notes

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,20 @@
-export function NoteList() {
-    return <div>note list</div>
+export function NoteList(props) {
+    if (! props.notes) return <section>Loading...</section>
+    return (
+        <section className="note-list">
+        {
+            props.notes.map(note =>
+                <article key={note.id}>
+                    <h3>{note.title}</h3>
+                    <p>{note.content}</p>
+                    <button
+                        className="delete"
+                        onClick={() => props.onDeleteNote(note.id)}
+                        >Delete
+                    </button>
+                </article>
+            )
+        }
+        </section>
+    )
 }
