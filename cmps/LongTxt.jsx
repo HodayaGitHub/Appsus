b38dc572a4eed9d8390efb211f5b6c32070bb3db
HodayaGitHub <hodaya1812@gmail.com>
2023-12-15 15:58:03 +0200

    Add dynamic class to LongTxt

--- cmps/LongTxt.jsx
+++ cmps/LongTxt.jsx
@@ -1,20 +1,20 @@
 const { useState } = React
 
-export function LongTxt({ txt , length}) {
+export function LongTxt({ txt, length, dynamicClass = '' }) {
   const [isLongTxtShown, setIsLongTxtShown] = useState(false)
 
-  function onToggleLongTxt () {
+  function onToggleLongTxt() {
     setIsLongTxtShown((prev) => !prev)
   }
 
-  function getTxtToShow (isLongTxtShown, txt) {
+  function getTxtToShow(isLongTxtShown, txt) {
     if (txt.length < length) return txt
     return isLongTxtShown ? txt : txt.substring(0, length) + '...'
   }
 
   const txtToShow = getTxtToShow(isLongTxtShown, txt)
   return (
-    <section className='longTxt'>
+    <section className={`longTxt ${dynamicClass}`}>
       <p>{txtToShow}</p>
       {/* {txt.length > 100 && (
         <button onClick={onToggleLongTxt}>
