a1bb20e19984d6ec7135d22a217792c6a19fc2ac
HodayaGitHub <hodaya1812@gmail.com>
2023-12-17 00:24:06 +0200

    Add style to the menu btn and to email preview

--- apps/mail/cmps/EmailPreview.jsx
+++ apps/mail/cmps/EmailPreview.jsx
@@ -2,14 +2,29 @@ import { EmailActions } from './EmailActions.jsx'
 
 
 export function EmailPreview({ email, onRemoveEmail, onStarredEmail, onReadChange }) {
+
+    const [name] = email.from.split('@')
+
     return (
         // <article className="email-preview">
         <React.Fragment>
-            <div>
-                <span className="email-subject">{email.subject}</span>
-                <span>{email.body}</span>
+            <div class="email-preview">
+                
+                <div className="email-preview-title">
+                    <span className="email-subject">{email.subject}</span>
+                    <EmailActions email={email}
+                        onRemoveEmail={onRemoveEmail}
+                        onStarredEmail={onStarredEmail}
+                        onReadChange={onReadChange} />
+                </div>
 
-                <EmailActions email={email} onRemoveEmail={onRemoveEmail} onStarredEmail={onStarredEmail} onReadChange={onReadChange} />
+                <div className="sender-details">
+                    <span><strong>{name}</strong></span>
+                    <span>{`<${email.from}>`}</span>
+                </div>
+
+                <span className="email-body">{email.body}</span>
+
 
             </div>
         </React.Fragment>
--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,26 +1,39 @@
 import { AppSearch } from "./AppSearch.jsx"
 
 const { NavLink } = ReactRouterDOM
+const { useState } = React
 
 export function AppHeader(props) {
 
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    function toggleMenu() {
+        setMenuOpen(prevState => !prevState);
+    }
     return (
-        <header className="app-header full-width">
+        <header className={`app-header full-width ${isMenuOpen ? 'menu-open' : ''}`}>
+
+        {/* <header className="app-header full-width"> */}
             <label className="hamburger-menu">
-                <span>Menu</span>
+                {/* <span>Menu</span> */}
+                <div className="hamburger-menu-container" onClick={toggleMenu}>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                </div>
+
                 <input className="hamburger-menu-checkbox" type="checkbox" />
             </label>
             <NavLink className="app-logo" to="/">
                 <span>App Logo</span>
             </NavLink>
-        {
-            0 < Object.keys(props).length &&
-            <AppSearch
-                SVG_ICONS={props.SVG_ICONS}
-                onSetFilterBy={props.onSetFilterBy}
-                filterBy={props.filterBy}
-            />
-        }
+            {
+                0 < Object.keys(props).length &&
+                <AppSearch
+                    SVG_ICONS={props.SVG_ICONS}
+                    onSetFilterBy={props.onSetFilterBy}
+                    filterBy={props.filterBy}
+                />
+            }
             <NavLink to="/mail">Mail</NavLink>
             <NavLink to="/note">Note</NavLink>
             <NavLink className="user-img-container" to="/">
